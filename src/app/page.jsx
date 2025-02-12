28e01783e69e61ca63eb0300c763ad84d3bda34b
Obi-codes <76232034+Obi-codes@users.noreply.github.com>
2025-02-11 23:14:34 -0500

    Add files via upload

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,55 +1,102 @@
 "use client";
+import React from "react";
 import dynamic from "next/dynamic";
-import profile1 from "../../public/assets/profile1.png";
-const GalleryView = dynamic(
-  () => import("@/components/GalleryViewComponent/GalleryView"),
-  { ssr: false }
-);
+import profile1 from "../../../public/assets/DefaultPic.png";
+import { useState } from "react";
 const GalleryDetailed = dynamic(
-  () => import("@/components/GalleryDetailedComponent/GalleryDetailed"),
+  () => import("@/personComp/GalleryDetailed"),
   { ssr: false }
 );
 
-const HomePage = () => {
-  const authUser =
-    typeof localStorage !== "undefined" &&
-    JSON.parse(localStorage?.getItem("inyeUserData"));
-
-  const item = {
-    quantity: 3,
-    amount: 14,
-  };
-
+// import CreateProject from "src/adminComp/CreateProject/index.js";
+import OverViewBtn from "@/personComp/OverviewAndBtn";
+import style from './AdminHomePage.module.css'
+import NoReporting from "@/adminComp/NoReporting";
+import TrackingProgress from "@/adminComp/TrackingProgress";
+import {TodayDate} from "@/toolkit/actions";
+import {ProjectNameDisplay} from "@/toolkit/actions"
+const AdminHomePage = () => {
   const data = {
     id: 1,
     profilePic: profile1,
-    name: "Healthcare Project",
-    date: "24/08/2024",
+    name: ProjectNameDisplay(),
+    date: TodayDate(),           
     quantity: "30 Person",
-    amount: "$300",
+    amount: "$250",
   };
 
+  const adminButtonArray = [
+    {
+      id: 1,
+      title: "Gallery", //dashboard
+    },
+    {
+      id: 2,
+      title: "Reporting",
+    },
+  ];
+  const [selectedButton, setSelectedButton] = useState(1);
+  const [showTrackingProgress, setShowTrackingProgress] = useState(false);
+  const [showReporting, setShowReporting] = useState(false);
+  const [showDetailView, setShowDetailView] = useState(false);
+
+  const handleButtonClick = (buttonId) => {
+    setSelectedButton(buttonId);
+    setShowTrackingProgress(buttonId === 'Dashboard');
+
+  };
+  const handleNoReportingButtonClick = () => {
+    setShowTrackingProgress(true);
+  };
+  const handleBackFromDetailView = () => {
+    setShowDetailView(false); // Set state to hide the detailed view
+  };
+  const backButton = showDetailView ? (
+    <button className={style.backButton} onClick={handleBackFromDetailView}>{"< Back "}</button>
+  ) : null;
+
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        backgroundColor: "#141316",
-        height: "100%",
-        flex: 1,
-      }}
-    >
-      <div style={{ width: "90%", padding: "3vw", height: "100%" }}>
-        {authUser?.type === "admin" ? (
-          <GalleryDetailed authUser={authUser} item={data} />
-        ) : (
-          <GalleryView />
-        )}
+    <div style={{
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+      backgroundColor: "#141316",
+      height: "150vh",
+      flex: 1,
+    }}>
+
+      <div className={style.mainDiv} style={{ width: "90%", padding: "3vw" }}>
+        <OverViewBtn
+          btnArray={adminButtonArray}
+          overViewCss={style.overViewCss}
+          backButton={backButton}
+          selectedButton={selectedButton}
+          setSelectedButton={handleButtonClick}
+        />
+        {!showReporting ? (
+          <GalleryDetailed
+            showTrackingProgress={showTrackingProgress}
+            setShowTrackingProgress={setShowTrackingProgress}
+            selectedButton={selectedButton}
+            setSelectedButton={handleButtonClick}
+            item={data}
+            setShowReporting={setShowReporting}
+            setShowDetailView={setShowDetailView}
+          />
+        ) : showTrackingProgress ? (<TrackingProgress setShowTrackingProgress={setShowTrackingProgress} />)
+          : <NoReporting
+            showTrackingProgress={showTrackingProgress}
+            setShowTrackingProgress={setShowTrackingProgress}
+            selectedButton={selectedButton}
+            setSelectedButton={handleButtonClick}
+            item={data}
+            showDetailView={showDetailView}
+            setShowDetailView={setShowDetailView}
+            setShowReporting={setShowReporting} progressPage handleButtonClick={handleNoReportingButtonClick} />
+        }
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default AdminHomePage;
